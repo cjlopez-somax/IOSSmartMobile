d593361369c3fcfef7776e5f6286b26c1e9270e0
Christian Lopez <cjlopez@somax.cl>
2020-12-17 12:37:51 -0300

    Initial Commit

---
+++ SSmartMobile/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SSmartMobile
+//
+//  Created by Christian on 17-12-20.
+//  Copyright © 2020 Christian. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
